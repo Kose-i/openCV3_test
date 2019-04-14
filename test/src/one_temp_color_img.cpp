5e0bfaaf5dfc48626d39e609d5e690c4f575d2da
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-04-14 10:26:30 +0900

    Add test onetonecolor-imgfile

---
+++ test/src/one_temp_color_img.cpp
@@ -0,0 +1,13 @@
+#include <opencv2/opencv.hpp>
+
+cv::
+
+int main(int argc, char** argv) {
+  auto rowsize=50;
+  auto colsize=50;
+  cv::Scalar color(255,255,255);
+
+  cv::Mat one_tone_color = cv::Mat::zeros(rowsize, colsize, CV_8UC3, color);
+  cv::imshow("one_tone_color", one_tone_color);
+  cv::waitKey(1);
+}
