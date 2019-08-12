de6240b28e3f7bf7968e3e3ea05b3fec6b93ba25
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-08-12 18:11:08 +0900

    Problem4-2

--- chapter4/src/problem_book.cpp
+++ chapter4/src/problem_book.cpp
@@ -2,9 +2,13 @@
 
 #include <iostream>
 
+void color_change(cv::Mat& data_map) {
+}
+
 void problem4_1() {
   cv::Mat temp = cv::Mat::zeros(500, 500, CV_8UC1);
   cv::namedWindow("temp", cv::WINDOW_NORMAL);
+  cv::Point pos();
   while (true) {
     cv::imshow("temp", temp);
     auto key = cv::waitKey(1);
@@ -19,19 +23,31 @@ void problem4_1() {
     } else if (key == 55) { // Number 7 push
     } else if (key == 56) { // Number 8 push
     } else if (key == 57) { // Number 9 push
-    } else if (key == 8) {          // BackSpace
-    } else if (key == 13) {  // Enter Key
-    } else if (key == 81) {  // Left Key
-    } else if (key == 82) {  // Up key
-    } else if (key == 83) {  // Right Key
-    } else if (key == 84) {  // Down Key
-    } else if (key == 'q') {
+    } else if (key == 8)  { // BackSpace
+    } else if (key == 13) { // Enter Key
+    } else if (key == 81) { // Left Key
+    } else if (key == 82) { // Up key
+    } else if (key == 83) { // Right Key
+    } else if (key == 84) { // Down Key
+    } else if (key == 32) { // Space Key
+      color_change(temp);
+    } else if (key == 'q') {//Quit push
       break;
     }
   }
 }
 
-void problem4_2() {}
+void problem4_2() {
+  cv::Mat temp(100, 200, CV_8UC1);
+  cv::RNG rng;
+  rng.fill(temp, cv::RNG::UNIFORM, 0, 255);
+  cv::imshow("temp", temp);
+  cv::destoryWindow("temp");
+
+  cv::Mat temp_con = cv::Mat::zeros(100, 200, CV_32FC1);
+  cv::imshow("temp_con", temp_con);
+  while (cv::waitKey(1) != 'q');
+}
 
 int main(int argc, char** argv) {
   problem4_1();
